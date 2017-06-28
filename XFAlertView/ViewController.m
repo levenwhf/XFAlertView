0f4fd6a037e4b1e3d90d89ac90d26a14da2466c5
weihongfang <weihf@weihfdeMac-mini.local>
2017-06-28 16:05:15 +0800

    Initial Commit

---
+++ XFAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XFAlertView
+//
+//  Created by weihongfang on 2017/6/28.
+//  Copyright © 2017年 Leven Wei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
